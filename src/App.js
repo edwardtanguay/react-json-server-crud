4897c308b8ed17afece890bef9dda00985eb5098
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-17 22:59:26 +0100

    show flashcard number

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 
+const url = 'http://localhost:5000/flashcards';
+
 function App() {
-  return (
-    <div className="App">
-		<p>Hello</p>
-    </div>
-  );
+	const [flashcards, setFlashcards] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(url);
+			const flashcards = await response.json();
+			setFlashcards(flashcards);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>{flashcards.length} Flashcards</h1>
+		</div>
+	);
 }
 
 export default App;
